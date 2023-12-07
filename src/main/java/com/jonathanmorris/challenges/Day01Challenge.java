b51e3296ee342a8e6cf246b75e69bfdd8d4ef542
Jonathan Morris <jonathanmorris@spotify.com>
2023-12-06 21:10:59 -0500

    Add initial solution

--- src/main/java/com/jonathanmorris/challenges/Day01Challenge.java
+++ src/main/java/com/jonathanmorris/challenges/Day01Challenge.java
@@ -1,5 +1,7 @@
 package com.jonathanmorris.challenges;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -13,6 +15,33 @@ public class Day01Challenge implements Challenge {
 
     @Override
     public void execute(final List<String> lines) {
-        logger.debug("Lines: {}", lines);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.toString();
+        String firstNumber = "";
+        String lastNumber = "";
+        for (String line : lines) {
+            logger.info("Current line is: {}", line);
+            List<String> chars = Arrays.asList(line.trim().split(""));
+            for (String currentChar : chars) {
+                if (isNumber(currentChar)) {
+                    firstNumber = currentChar;
+                    break;
+                }
+            }
+            for (int i = chars.size() - 1; i >= 0; i--) {
+                String currentChar = chars.get(i);
+                if (isNumber(currentChar)) {
+                    lastNumber = currentChar;
+                    break;
+                }
+            }
+            numbers.add(Integer.parseInt(String.format("%s%s", firstNumber, lastNumber)));
+        }
+        int result = numbers.stream().reduce(0, Integer::sum);
+        logger.info("Result is: {}", result);
+    }
+
+    private Boolean isNumber(final String currentChar) {
+        return currentChar.matches("[0-9]");
     }
 }
