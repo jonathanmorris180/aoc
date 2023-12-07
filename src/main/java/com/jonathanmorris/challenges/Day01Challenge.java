fe2b9659e97170052c8092178cc063b03bfb681a
Jonathan Morris <jonathanmorris@spotify.com>
2023-12-07 13:01:06 -0500

    Finish solution for day 1

--- src/main/java/com/jonathanmorris/challenges/Day01Challenge.java
+++ src/main/java/com/jonathanmorris/challenges/Day01Challenge.java
@@ -2,11 +2,8 @@ package com.jonathanmorris.challenges;
 
 import com.jonathanmorris.Challenge;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,89 +14,37 @@ public class Day01Challenge implements Challenge {
       Map.of(
           "one", 1, "two", 2, "three", 3, "four", 4, "five", 5, "six", 6, "seven", 7, "eight", 8,
           "nine", 9);
-  private static final String WORDS_TO_NUMBER_REGEX = String.join("|", WORDS_TO_NUMBER.keySet());
-  private static final List<String> ORDERED_NUMS =
-      Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
 
   @Override
   public void execute(final List<String> lines) {
     List<Integer> numbers = new ArrayList<>();
-    numbers.toString();
-    String firstNumber = "";
-    String lastNumber = "";
     for (String line : lines) {
-      line = this.replaceWithNumbers(line);
       logger.info("Line is: {}", line);
-      List<String> chars = Arrays.asList(line.trim().split(""));
-      for (String currentChar : chars) {
-        if (this.isNumber(currentChar)) {
-          firstNumber = currentChar;
-          break;
-        }
-      }
-      for (int i = chars.size() - 1; i >= 0; i--) {
-        String currentChar = chars.get(i);
-        if (this.isNumber(currentChar)) {
-          lastNumber = currentChar;
-          break;
-        }
-      }
-      numbers.add(Integer.parseInt(String.format("%s%s", firstNumber, lastNumber)));
+      List<Integer> numberStrings = getNumbersFrom(line);
+      List<Integer> firstAndLast = List.of(numberStrings.get(0), numberStrings.get(numberStrings.size() - 1));
+      String numberString =
+          String.join("", firstAndLast.stream().map(String::valueOf).toArray(String[]::new));
+      logger.info("Number string is: {}", numberString);
+      numbers.add(Integer.parseInt(numberString));
     }
     int result = numbers.stream().reduce(0, Integer::sum);
     logger.info("Result is: {}", result);
   }
 
-  private Boolean isNumber(final String currentChar) {
-    return currentChar.matches("[0-9]");
-  }
-
-  private String replaceWithNumbers(final String line) {
-    Pattern pattern = Pattern.compile(this.addGroup(WORDS_TO_NUMBER_REGEX));
-    Matcher matcher = pattern.matcher(line);
-
-    StringBuffer buffer = new StringBuffer();
-    if (matcher.find()) {
-
-      List<Integer> numbersToAdd = new ArrayList<>();
-      int previousEnd = 0;
-      String previousNumber = "";
-      boolean isOverlapping = false;
-      do {
-        String match = matcher.group();
-        logger.info("Match is: {}", match);
-        if (!isOverlapping && !numbersToAdd.isEmpty()) {
-          logger.info("resetting list");
-          numbersToAdd = new ArrayList<>();
+  private List<Integer> getNumbersFrom(final String line) {
+    List<Integer> numbers = new ArrayList<>();
+    for (int i = 0; i < line.length(); i++) {
+      String currentString = line.substring(i);
+      String firstChar = String.valueOf(currentString.charAt(0));
+      for (String numberWord : WORDS_TO_NUMBER.keySet()) {
+        if (currentString.startsWith(numberWord)) {
+          numbers.add(WORDS_TO_NUMBER.get(numberWord));
         }
-        if (previousEnd != 0 && previousEnd >= matcher.start()) {
-          logger.info("overlap found");
-          isOverlapping = true;
-          numbersToAdd.add(WORDS_TO_NUMBER.get(match));
-          previousEnd = matcher.end();
-          continue;
-        } else if (!isOverlapping && previousEnd != 0 && previousEnd < matcher.start()) {
-          logger.info("adding previous value");
-          matcher.appendReplacement(buffer, String.valueOf(WORDS_TO_NUMBER.get(previousNumber)));
-        }
-        if (!isOverlapping) {
-          logger.info("default");
-          previousNumber = match;
-        }
-        previousEnd = matcher.end();
-      } while (matcher.find(matcher.start() + 1));
-      if (isOverlapping) {
-        logger.info("adding value from overlap");
-        String replacement = numbersToAdd.stream().map(String::valueOf).reduce("", String::concat);
-        matcher.appendReplacement(buffer, replacement);
-        isOverlapping = false;
+      }
+      if (firstChar.matches("[0-9]")) {
+        numbers.add(Integer.parseInt(firstChar));
       }
     }
-    matcher.appendTail(buffer);
-    return buffer.toString();
-  }
-
-  private String addGroup(final String line) {
-    return String.format("(%s)", line);
+    return numbers;
   }
 }
