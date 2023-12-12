1f3586611abae162ddc7f6c318cc548eb70ceeb9
Jonathan Morris <jonathanmorris@spotify.com>
2023-12-12 18:16:56 -0500

    Add boilerplate for day 4

---
+++ src/main/java/com/jonathanmorris/challenges/Day04Challenge.java
@@ -0,0 +1,15 @@
+package com.jonathanmorris.challenges;
+
+import java.util.List;
+
+import com.jonathanmorris.Challenge;
+
+public class Day04Challenge implements Challenge {
+
+    @Override
+    public void execute(List<String> lines) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+}
