824ba7c34987ce3fee4b549d54b8f56322b43da4
Jonathan Morris <jonathanmorris@spotify.com>
2023-12-12 22:37:38 -0500

    Add initial solution for day 5 part one

--- src/main/java/com/jonathanmorris/challenges/Day05Challenge.java
+++ src/main/java/com/jonathanmorris/challenges/Day05Challenge.java
@@ -1,17 +1,141 @@
 package com.jonathanmorris.challenges;
 
 import com.jonathanmorris.Challenge;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Day05Challenge implements Challenge {
 
   private static final Logger logger = LoggerFactory.getLogger(Day01Challenge.class);
+  private static final String MAP = "map";
+  private static final List<String> MAPS =
+      List.of(
+          "seed-to-soil",
+          "soil-to-fertilizer",
+          "fertilizer-to-water",
+          "water-to-light",
+          "light-to-temperature",
+          "temperature-to-humidity",
+          "humidity-to-location");
 
   @Override
   public void execute(List<String> lines) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    List<Integer> seeds =
+        Arrays.stream(lines.get(0).split(":")[1].trim().split(" ")).map(Integer::valueOf).toList();
+    Map<String, GardenerMap> gardenerMaps = new HashMap<>();
+    List<Integer> locations = new ArrayList<>();
+    String mapName = "";
+    for (String line : lines) {
+      if (line.contains(MAP)) {
+        mapName = line.split(MAP)[0].trim();
+        GardenerMap map = new GardenerMap(mapName);
+        gardenerMaps.put(map.getName(), map);
+      } else if (line.trim().length() > 0 && mapName.length() > 0) {
+        GardenerMap currentMap = gardenerMaps.get(mapName);
+        currentMap.addLine(line);
+      }
+    }
+    this.parse(gardenerMaps.values());
+    for (Integer seed : seeds) {
+      Integer value = 0;
+      for (String map : MAPS) {
+        GardenerMap gardenerMap = gardenerMaps.get(map);
+        if (value == 0) {
+          value = gardenerMap.getValue(seed);
+          continue;
+        }
+        value = gardenerMap.getValue(value);
+      }
+      locations.add(value);
+    }
+    logger.debug("Answer for part one is {}", locations.stream().reduce(0, Integer::min));
+  }
+
+  private void parse(Collection<GardenerMap> gardenerMaps) {
+    for (GardenerMap map : gardenerMaps) {
+      map.parseLines();
+      map.completeMapping();
+    }
+  }
+
+  private class GardenerMap {
+    private final String name;
+    private final List<Line> lines;
+    private final Map<Integer, Integer> mappedValues;
+
+    private GardenerMap(String name) {
+      this.name = name;
+      this.lines = new ArrayList<>();
+      this.mappedValues = new HashMap<>();
+    }
+
+    public void addLine(String line) {
+      this.lines.add(new Line(line));
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public Integer getValue(Integer val) {
+      return this.mappedValues.get(val);
+    }
+
+    public void parseLines() {
+      for (Line line : lines) {
+        Integer sourceNum = line.getSourceNum();
+        Integer destNum = line.getDestNum();
+        for (int i = 0; i < line.getRangeLength(); i++) {
+          this.mappedValues.put(sourceNum, destNum);
+          sourceNum++;
+          destNum++;
+        }
+      }
+    }
+
+    public void completeMapping() {
+      for (Line line : lines) {
+        for (int i = 0; i < line.getMaxSourceVal(); i++) {
+          if (this.mappedValues.get(i) == null) {
+            this.mappedValues.put(i, i);
+          }
+        }
+      }
+    }
+  }
+
+  private class Line {
+    private Integer destNum;
+    private Integer sourceNum;
+    private Integer rangeLength;
+
+    private Line(String line) {
+      List<Integer> numbers = Arrays.stream(line.split(" ")).map(Integer::valueOf).toList();
+      this.destNum = numbers.get(0);
+      this.sourceNum = numbers.get(1);
+      this.rangeLength = numbers.get(2);
+    }
+
+    public Integer getRangeLength() {
+      return this.rangeLength;
+    }
+
+    public Integer getSourceNum() {
+      return this.sourceNum;
+    }
+
+    public Integer getDestNum() {
+      return this.destNum;
+    }
+
+    public Integer getMaxSourceVal() {
+      return this.sourceNum + this.rangeLength;
+    }
   }
 }
